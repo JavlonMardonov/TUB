58122e666382056bd522198efc48a000cebf138e
Javlon <159873725+JavlonMardonov@users.noreply.github.com>
2024-05-25 09:58:10 +0500

    Add files via upload

---
+++ TUB.py
@@ -0,0 +1,11 @@
+a = int(input())
+count = 0
+n = 2
+while n <= sqrt(a):
+    if a % n == 0:
+        count = count + 1
+    n = n + 1
+if count != 0:
+    print("Tub emas")
+else:
+    print("Tub")
